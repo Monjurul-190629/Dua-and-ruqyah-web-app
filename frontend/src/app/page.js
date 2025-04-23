9c7c9659dba8d5910fd45b581d7ff483cabe0a2f
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-23 12:29:26 +0600

    category heading is added

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,20 +1,35 @@
+'use client'
+
 import Image from "next/image";
 import Loading from "./loading";
 import SectionHeader from "@/Component/SectionHeader";
+import CategoryHeading from "@/Component/CategoryHeading";
+import { useState } from "react";
 
 export default function Home() {
+  
+  // For search
+  const [searchTerm, setSearchTerm] = useState(''); 
+
   return (
     <div>
+      {/* Category Header */}
       <div className='md:px-2 md:pt-5 pt-5'>
-         <SectionHeader/>
+        <SectionHeader />
       </div>
 
       <div className="flex flex-col lg:flex-row justify-center gap-14 mt-4">
+
+        <div className='w-[350px]'>
+          
+          <CategoryHeading searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+
+        </div>
 
 
       </div>
     </div>
 
-    
+
   );
 }
